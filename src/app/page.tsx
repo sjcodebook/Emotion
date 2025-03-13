912499f4d17419aab6b155cc80f941f6eb4f67f1
0xSJ <jainsanmati846@gmail.com>
2025-03-13 13:59:35 +0530

    Refactor code style and add logo SVG asset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import Image from 'next/image'
+
 import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <div>
+    <div className='h-full'>
+      <h1 className='text-4xl font-bold'>Hello World</h1>
+      <Image src='assets/svg/logo.svg' alt='logo' width={200} height={200} />
       <Button>Click me</Button>
     </div>
   )
