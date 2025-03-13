87e7bcde0e41d7a531faec67cb2dd8c4aec68306
0xSJ <jainsanmati846@gmail.com>
2025-03-13 15:00:37 +0530

    Add theme provider and mode toggle component for theme management

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
+import { ThemeProvider } from '@/providers/theme-provider'
+
 const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
@@ -19,8 +21,16 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
-      <body className={`${inter.className} antialiased`}>{children}</body>
+    <html lang='en' suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 
 import { Button } from '@/components/ui/button'
+import { ModeToggle } from '@/components/mode-toggle'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <h1 className='text-4xl font-bold'>Hello World</h1>
       <Image src='assets/svg/logo.svg' alt='logo' width={200} height={200} />
       <Button>Click me</Button>
+      <ModeToggle />
     </div>
   )
 }
