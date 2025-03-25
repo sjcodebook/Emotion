ebdd34f2c15511362939234f30b6c0896a71013f
0xSJ <jainsanmati846@gmail.com>
2025-03-25 12:16:02 +0530

    Add Workspace model to Prisma schema; update Home component to fetch and display users; remove unused dashboard layout and page components; create Prisma client instance

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,13 +1,22 @@
-import Image from 'next/image'
+import prisma from '@/lib/prisma'
 
-import { Button } from '@/components/ui/button'
-import { ModeToggle } from '@/components/mode-toggle'
-import Navbar from './navbar'
+export default async function Home() {
+  const users = await prisma.user.findMany()
 
-export default function Home() {
+  console.log(users)
+
   return (
-    <div className='h-full'>
-      <Navbar />
+    <div className='min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16'>
+      <h1 className='text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]'>
+        Superblog dd
+      </h1>
+      <ol className='list-decimal list-inside font-[family-name:var(--font-geist-sans)] text-[#333333]'>
+        {users.map((user) => (
+          <li key={user.id} className='mb-2'>
+            {user.name}
+          </li>
+        ))}
+      </ol>
     </div>
   )
 }
