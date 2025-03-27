7ec676d652a71ea9ba2d1a7fbcf4c9013ce76c31
0xSJ <jainsanmati846@gmail.com>
2025-03-27 17:36:20 +0530

    Add session and JWT callbacks to NextAuth for user ID management

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,6 +5,18 @@ import prisma from '@/lib/prisma'
 import authConfig from '@/lib/auth.config'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if (token?.sub && session?.user) {
+        session.user.id = token.sub
+      }
+
+      return session
+    },
+    async jwt({ token }) {
+      return token
+    },
+  },
   adapter: PrismaAdapter(prisma),
   session: { strategy: 'jwt' },
   ...authConfig,
