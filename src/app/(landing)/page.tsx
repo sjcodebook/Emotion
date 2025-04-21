98d84f4a4dd5c0995ed1cda1f3d95e021ec4a8a8
0xSJ <jainsanmati846@gmail.com>
2025-04-21 11:40:05 +0530

    feat: add Dockerfile and .dockerignore; refactor landing page to redirect to dashboard

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,35 +1,14 @@
-import { signIn } from '@/lib/auth'
+'use client'
 
-const Landing = async () => {
-  return (
-    <div className='min-h-full flex flex-col'>
-      {/* <div className='flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10'>
-        <Heading />
-      </div> */}
-      <form
-        action={async (formData) => {
-          'use server'
-          try {
-            const ll = await signIn('credentials', formData)
-            console.log('ss========')
-            console.log(ll)
-          } catch (error) {
-            console.log('ss========')
-            console.error(error)
-          }
-        }}>
-        <label>
-          Email
-          <input name='email' type='email' />
-        </label>
-        <label>
-          Password
-          <input name='password' type='password' />
-        </label>
-        <button>Sign In</button>
-      </form>
-    </div>
-  )
+import { useEffect } from 'react'
+import { redirect } from 'next/navigation'
+
+const Landing = () => {
+  useEffect(() => {
+    redirect('/dashboard')
+  }, [])
+
+  return null
 }
 
 export default Landing
